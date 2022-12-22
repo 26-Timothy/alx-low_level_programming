1162a7adabd6569aa13def289336f975929eef6f
Timothy Munyao <timothykyovo@gmail.com>
2022-12-22 04:28:27 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+	puts("\"programmming is like building a multilingual puzzle");
+	 return (0);
+
+}
