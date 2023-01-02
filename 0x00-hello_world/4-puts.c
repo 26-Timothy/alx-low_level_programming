ae48fc4b26868bb4f492fa3bd08e61419d5e1b44
Timothy Munyao <timothykyovo@gmail.com>
2023-01-02 04:36:31 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 /**
- * main - programm that print line with put function
- * return : 0 (success)
+ * main - A program that print line with put function
+ * Return : 0 (success)
  */
 int main(void)
-
 {
-puts("\"programmming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 
 }
